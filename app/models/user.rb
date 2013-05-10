30cd603fb59e66768d7914e5a77d138a2ff337a3
Pieter Moorman <pietermoorman@hotmail.com>
2013-05-10 09:01:06 +0200

    add profileImage with paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,15 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :image
   # attr_accessible :title, :body
+
+  validates_attachment :image,
+                          content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
+                          size: { less_than: 5.megabytes }
 
    # Connects viewers to their respective users. 
   # Destroys dependent viewers when user is destroyed
   has_many :viewers, :dependent => :destroy
+  has_attached_file :image, styles: { medium: "200x200"}
 end
