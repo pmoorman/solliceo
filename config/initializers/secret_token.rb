ed524e3c5ef6e9bd79c544f34b848eb3d76e903b
Pieter Moorman <pietermoorman@hotmail.com>
2013-05-10 07:04:26 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Solliceo::Application.config.secret_token = 'c6bbc6d7236af9d815e0d0acda30686324e44e24a3b50602e026485f3eb3cdf24502b8b1f45a5266ec3ce2aba803623f7cb76a03b637b7d322186583da8b48ec'
