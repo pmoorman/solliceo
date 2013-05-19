489a89417f08b7523b07524ed3e7db90bde615bf
Paul Ser <pashka4281@gmail.com>
2013-05-19 17:37:40 +0300

    look and feel upd

---
+++ app/controllers/files_controller.rb
@@ -0,0 +1,3 @@
+class FilesController < ApplicationController
+
+end
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -1,13 +1,10 @@
 class Viewer < ActiveRecord::Base
   attr_accessible :company, :vacancy, :resume
 
-  validates :company, presence: true
-  validates :vacancy, presence: true
-  validates_attachment :resume,
-                            content_type: { content_type: ['resume/pdf'] }
+  validates :company, :vacancy, :user, presence: true
+  validates_attachment :resume, content_type: { content_type: %w(application/pdf resume/pdf) }
 
   # Connects viewers to their respective user
   belongs_to :user
-  validates :user_id, presence: true
   has_attached_file :resume
 end
