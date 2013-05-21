ae314e1b104a69babea57df68fce92e92b74f17a
Paul Ser <pashka4281@gmail.com>
2013-05-21 14:48:41 +0300

    file upload path fix

--- app/models/file_upload.rb
+++ app/models/file_upload.rb
@@ -4,7 +4,7 @@ class FileUpload < ActiveRecord::Base
 
   attr_accessible :file, :name, :viewer_id, :viewer
   has_attached_file :file, 
-  	#:url => "/uploaded_files/#{viewer_id}/:file_name",
+  	:url => "/uploaded_files/:viewer_id/:basename.:extension",
   	:path => ":rails_root/public/uploaded_files/:viewer_id/:basename.:extension"
   validates_attachment_presence :file
   validates_attachment :file, content_type: { content_type: %w(application/pdf resume/pdf) }
