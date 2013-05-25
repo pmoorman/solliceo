568383819b8267fd386c8771482a3f58970d30df
Paul Ser <pashka4281@gmail.com>
2013-05-25 17:14:18 +0300

    upload fix + new allowed mime types added

--- app/models/file_upload.rb
+++ app/models/file_upload.rb
@@ -8,6 +8,15 @@ class FileUpload < ActiveRecord::Base
   	:url => "/uploaded_files/:viewer_id/:basename.:extension",
   	:path => ":rails_root/public/uploaded_files/:viewer_id/:basename.:extension"
   validates_attachment_presence :file
-  validates_attachment :file, content_type: { content_type: %w(application/pdf resume/pdf) }
+  validates_attachment :file, content_type: { content_type: 
+  	[   'application/pdf', 
+  		'resume/pdf', 
+  		'application/msword',
+  		'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+  		'application/vnd.ms-powerpoint',
+  		'application/vnd.openxmlformats-officedocument.presentationml.presentation'  
+  	] 
+  }
+  #other mime types can be found here : http://filext.com/faq/office_mime_types.php
 
 end
