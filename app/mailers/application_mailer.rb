14c4b2b92cd3f56f5a0aec1673439abc50b5acc6
Nikita Gnotovs <n.gnotov@gmail.com>
2013-08-27 21:10:36 +0300

    mandrill custom template probable fix

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -18,16 +18,18 @@ class ApplicationMailer
           "from_email" => application.from,
           "subject" => application.subject,
           "to" => application.emails_clean.map{|email| {email: email}},
+          "html" => application.html_body.present? ? application.html_body : '',
+          "text" => application.text_body.present? ? application.text_body : ''
       }
       async = false
       ip_pool = "Main Pool"
       result = mandrill.messages.send_template params, message, async, ip_pool
       result.map {|r| "#{r['email']}: #{r['status']}"}.to_sentence
-    #rescue Mandrill::Error => e
+    rescue Mandrill::Error => e
       # Mandrill errors are thrown as exceptions
-    #  puts "A mandrill error occurred: #{e.class} - #{e.message}"
+      puts "A mandrill error occurred: #{e.class} - #{e.message}"
       # A mandrill error occurred: Mandrill::InvalidKeyError - Invalid API key
-    #  raise
+      raise
     end
   end
 end
